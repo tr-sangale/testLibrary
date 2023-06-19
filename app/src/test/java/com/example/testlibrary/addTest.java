4d3fb26645e26a082c4a9f3305e35adb925f4b99
tr-sangale <86203327+tr-sangale@users.noreply.github.com>
2023-06-19 16:44:40 +0530

    Update addTest.java

--- app/src/test/java/com/example/testlibrary/addTest.java
+++ app/src/test/java/com/example/testlibrary/addTest.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class LibraryTest {
+public class addTest {
     @Test
     public void testAddNumbers() {
         int result = Library.addNumbers(2, 3);
